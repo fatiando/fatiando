c9adaee790dff7bc0904c013ecbb58c8823b69ca
Leonardo Uieda <leouieda@gmail.com>
2010-08-18 17:15:11 -0300

    Ported InterG2D to GradientSolver
    --HG--
    rename : fatiando/geoinv/__init__.py => fatiando/inversion/__init__.py
    rename : fatiando/geoinv/climatesignal.py => fatiando/inversion/climatesignal.py
    rename : fatiando/geoinv/fullwave.py => fatiando/inversion/fullwave.py
    rename : fatiando/geoinv/gradientsolver.py => fatiando/inversion/gradientsolver.py
    rename : fatiando/geoinv/interg.py => fatiando/inversion/interg.py
    rename : fatiando/geoinv/linearsolver.py => fatiando/inversion/linearsolver.py
    rename : fatiando/geoinv/lmsolver.py => fatiando/inversion/lmsolver.py
    rename : fatiando/geoinv/pgrav.py => fatiando/inversion/pgrav.py
    rename : fatiando/geoinv/simpletom.py => fatiando/inversion/simpletom.py
    rename : fatiando/geoinv/tests/__init__.py => fatiando/inversion/tests/__init__.py

--- fatiando/tests/__init__.py
+++ fatiando/tests/__init__.py
@@ -7,7 +7,7 @@ __date__ = 'Created 29-Mar-2010'
 import unittest
 
 import fatiando.directmodels.tests
-import fatiando.geoinv.tests
+import fatiando.inversion.tests
 import fatiando.math.tests
 import fatiando.utils.tests
 import fatiando.data.tests
@@ -18,7 +18,7 @@ def suite(label='fast'):
     testsuite = unittest.TestSuite()
 
     testsuite.addTest(fatiando.directmodels.tests.suite(label))
-    testsuite.addTest(fatiando.geoinv.tests.suite(label))
+    testsuite.addTest(fatiando.inversion.tests.suite(label))
     testsuite.addTest(fatiando.math.tests.suite(label))
     testsuite.addTest(fatiando.utils.tests.suite(label))
     testsuite.addTest(fatiando.data.tests.suite(label))
