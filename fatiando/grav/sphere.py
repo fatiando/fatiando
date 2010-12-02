a39583f33fe80a2512bf4b35d43b0f8507ef19ff
Leonardo Uieda <leouieda@gmail.com>
2010-12-02 20:18:19 -0200

    Implemented diagonal tensor components for the sphere

--- fatiando/grav/sphere.py
+++ fatiando/grav/sphere.py
@@ -23,6 +23,15 @@ Functions:
 * :func:`fatiando.grav.sphere.gz`
     Calculates the :math:`g_z` gravity component.
 
+* :func:`fatiando.grav.sphere.gxx`
+    Calculates the :math:`g_xx` gravity gradient tensor component.
+
+* :func:`fatiando.grav.sphere.gyy`
+    Calculates the :math:`g_yy` gravity gradient tensor component.
+
+* :func:`fatiando.grav.sphere.gzz`
+    Calculates the :math:`g_zz` gravity gradient tensor component.
+
 """
 __author__ = 'Leonardo Uieda (leouieda@gmail.com)'
 __date__ = 'Created 30-Nov-2010'
@@ -72,6 +81,114 @@ def gz(dens, radius, xc, yc, zc, xp, yp, zp):
     """
 
     res = sphere_ext.sphere_gz(float(dens), float(radius), float(xc), float(yc),
-                            float(zc), float(xp), float(yp), float(zp))
+                               float(zc), float(xp), float(yp), float(zp))
+
+    return res
+
+
+def gxx(dens, radius, xc, yc, zc, xp, yp, zp):
+    """
+    Calculates the :math:`g_xx` gravity gradient tensor component.
+
+    The coordinate system of the input parameters is to be x -> North,
+    y -> East and z -> **DOWN**.
+
+    **NOTE**: All input values in **SI** units(!) and output in **Eotvos**!
+
+    Parameters:
+
+    * dens
+        Density of the sphere
+
+    * radius
+        Radius of the sphere
+
+    * xc, yc, zc
+        Coordinates of the center of the sphere
+
+    * xp, yp, zp
+        Coordinates of the point **P** where the field will be calculated
+
+    Returns:
+
+    * the :math:`g_xx` component calculated at **P**
+
+    """
+
+    res = sphere_ext.sphere_gxx(float(dens), float(radius), float(xc),
+                                float(yc), float(zc), float(xp), float(yp),
+                                float(zp))
+
+    return res
+
+
+def gyy(dens, radius, xc, yc, zc, xp, yp, zp):
+    """
+    Calculates the :math:`g_yy` gravity gradient tensor component.
+
+    The coordinate system of the input parameters is to be x -> North,
+    y -> East and z -> **DOWN**.
+
+    **NOTE**: All input values in **SI** units(!) and output in **Eotvos**!
+
+    Parameters:
+
+    * dens
+        Density of the sphere
+
+    * radius
+        Radius of the sphere
+
+    * xc, yc, zc
+        Coordinates of the center of the sphere
+
+    * xp, yp, zp
+        Coordinates of the point **P** where the field will be calculated
+
+    Returns:
+
+    * the :math:`g_yy` component calculated at **P**
+
+    """
+
+    res = sphere_ext.sphere_gyy(float(dens), float(radius), float(xc),
+                                float(yc), float(zc), float(xp), float(yp),
+                                float(zp))
+
+    return res
+
+
+def gzz(dens, radius, xc, yc, zc, xp, yp, zp):
+    """
+    Calculates the :math:`g_zz` gravity gradient tensor component.
+
+    The coordinate system of the input parameters is to be x -> North,
+    y -> East and z -> **DOWN**.
+
+    **NOTE**: All input values in **SI** units(!) and output in **Eotvos**!
+
+    Parameters:
+
+    * dens
+        Density of the sphere
+
+    * radius
+        Radius of the sphere
+
+    * xc, yc, zc
+        Coordinates of the center of the sphere
+
+    * xp, yp, zp
+        Coordinates of the point **P** where the field will be calculated
+
+    Returns:
+
+    * the :math:`g_zz` component calculated at **P**
+
+    """
+
+    res = sphere_ext.sphere_gzz(float(dens), float(radius), float(xc),
+                                float(yc), float(zc), float(xp), float(yp),
+                                float(zp))
 
     return res
