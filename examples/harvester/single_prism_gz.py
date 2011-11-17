9a9c9777f4f05e08c65cc7ce2d54986e32dcdf07
Leonardo Uieda <leouieda@gmail.com>
2011-11-17 20:15:27 -0200

    Harvester: finished example script; data module classes (only gz); loadseeds (need test); sow

--- examples/harvester/single_prism_gz.py
+++ examples/harvester/single_prism_gz.py
@@ -7,7 +7,7 @@ except ImportError:
     from enthought.mayavi import mlab
 from matplotlib import pyplot
 from fatiando import potential, vis, logger, utils, gridder
-from fatiando.mesher.volume import Prism3D, PrismMesh3D, extract
+from fatiando.mesher.volume import Prism3D, PrismMesh3D, extract, vfilter
 from fatiando.inversion import harvester
 
 vis.mlab = mlab
@@ -29,21 +29,24 @@ mlab.show()
 
 log.info("\nSecond calculate the synthetic data:")
 shape = (100,100)
-x, y, z = gridder.scatter(extent[0:4], 200, z=-1)
+#x, y, z = gridder.scatter(extent[0:4], 200, z=-1)
+x, y, z = gridder.regular(extent[0:4], shape, z=-1)
 gz = utils.contaminate(potential.prism.gz(x, y, z, model), 0.1)
 
 pyplot.figure()
 pyplot.axis('scaled')
 pyplot.title('Synthetic gz data')
-levels = vis.contourf(y, x, gz, shape, 10, interpolate=True)
-vis.contour(y, x, gz, shape, levels, interpolate=True)
+#levels = vis.contourf(y, x, gz, shape, 10, interpolate=True)
+#vis.contour(y, x, gz, shape, levels, interpolate=True)
+levels = vis.contourf(y, x, gz, shape, 10)
+vis.contour(y, x, gz, shape, levels)
 pyplot.plot(y, x, 'xk')
 pyplot.xlabel('East (km)')
 pyplot.ylabel('North (km)')
 pyplot.show()
 
 log.info("\nThird make a prism mesh:")
-mesh = PrismMesh3D(extent, (50, 50, 30))
+mesh = PrismMesh3D(extent, (30, 50, 50))
 
 mlab.figure(bgcolor=(1,1,1))
 vis.prisms3D(mesh, (0 for i in xrange(mesh.size)))
@@ -52,10 +55,40 @@ vis.add_axes3d(outline)
 mlab.show()
 
 log.info("\nFourth sow the seeds:")
-seeds = [harvester.sow(mesh, (5000, 5000, 3000), {'density':800})]
+rawseeds = [((5000, 5000, 3000), {'density':800})]
+seeds = harvester.sow(mesh, rawseeds)
 
+mlab.figure(bgcolor=(1,1,1))
+vis.prisms3D(model, extract('density', model), opacity=0.3)
+vis.prisms3D((mesh[s] for s, p in seeds), (p['density'] for s, p in seeds))
+outline = mlab.outline(color=(0,0,0), extent=extent)
+vis.add_axes3d(outline)
+vis.wall_bottom(extent)
+vis.wall_north(extent)
+mlab.show()
+    
 log.info("\nFith harvest the results:")
-datamods = [harvester.GzModule(x, y, z, gz)]
-mesh = harvester.harvest(seeds, mesh, datamods)
+gzmod = harvester.GzModule(x, y, z, gz)
+estimate = harvester.harvest(seeds, mesh, [gzmod])
+for prop in estimate:
+    mesh.addprop(prop, estimate[prop])
+density_model = vfilter(1, 2000, 'density', mesh)
 
-log.info("\nSixth plot things:")
+pyplot.figure()
+pyplot.title("Adjustment")
+pyplot.axis('scaled')
+levels = vis.contourf(y, x, gz, shape, 12)
+pyplot.colorbar()
+vis.contour(y, x, gzmod.predicted(), shape, levels)
+pyplot.xlabel('East (km)')
+pyplot.ylabel('North (km)')
+pyplot.show()
+
+mlab.figure(bgcolor=(1,1,1))
+vis.prisms3D(model, extract('density', model), style='wireframe')
+vis.prisms3D(density_model, extract('density', density_model))
+outline = mlab.outline(color=(0,0,0), extent=extent)
+vis.add_axes3d(outline)
+vis.wall_bottom(extent)
+vis.wall_north(extent)
+mlab.show()    
--- fatiando/inversion/harvester.py
+++ fatiando/inversion/harvester.py
@@ -20,30 +20,263 @@
 __author__ = 'Leonardo Uieda (leouieda@gmail.com)'
 __date__ = 'Created 17-Nov-2010'
 
+
+import numpy
+
 from fatiando import potential
 
 
 class DataModule(object):
     """
+    Mother of all data modules.
+
+    Parameters:
+    * norm
+        Order of the norm of the residual vector to use (Default: 2). Can be:
+        * 1 -> l1 norm
+        * 2 -> l2 norm
+        * etc
+            
     """
 
-    def __init__(self, norm='l2'):
-        pass
+    def __init__(self, norm=2):
+        self.norm = norm
 
-    def residuals(self):
-        pass
-
-    def misfit(self):
-        pass
-    
+    def misfit(self, residuals):
+        """
+        Return the data misfit given a residual vector.
         
+        Uses the prespecified norm.
 
+        Parameters:
+        * residuals
+            Array with the residuals calculated on each data point
 
-def sow(mesh, point, props):
+        Returns:
+        * float
+            The data misfit
+            
+        """
+        return numpy.linalg.norm(residuals, self.norm)
+
+    def residuals(self, mesh, estimate, neighbor):
+        """
+        Calculate the residuals vector due to adding a neighbor to the estimate.
+
+        Parameters:
+        * mesh
+            A 3D mesh. See :mod:`fatiando.mesher.volume`
+        * estimate
+            Dictionary with the physical properties of the current estimate
+            without the neighbor
+        * neighbor
+            [n, props]
+            n is the index of the neighbor in the mesh.
+            props is a dictionary with the physical properties of the neighbor
+
+        Returns:
+        * array
+            Array with the residuals
+            
+        """
+        raise NotImplementedError("Method 'residuals' was not implemented")
+
+    def cleanup(self, neighbor):
+        """
+        Delete unnecessary things after neighbor has been added to the estimate.
+
+        Parameters:
+        * neighbor
+            [n, props]
+            n is the index of the neighbor in the mesh.
+            props is a dictionary with the physical properties of the neighbor
+        
+        """
+        pass
+                
+class PotentialModule(DataModule):
     """
-    Return seed
+    Mother class for potential field data modules
+
+    Remember: the coordinate system is x->North, y->East, and z->Down
+
+    Parameters:
+    * x, y, z
+        Arrays with the x, y, and z coordinates of the data points.
+    * obs
+        Array with the observed values of the component of the potential field.
+    * norm
+        Order of the norm of the residual vector to use (Default: 2). Can be:
+        * 1 -> l1 norm
+        * 2 -> l2 norm
+        * etc
+    
     """
-    pass
+
+    def __init__(self, x, y, z, obs, norm=2):
+        DataModule.__init__(self, norm)
+        self.x = x
+        self.y = y
+        self.z = z
+        self.res = numpy.array(obs)
+        self.obs = obs
+        self.effect = {}
+
+    def calc_effect(self, prop, x1, x2, y1, y2, z1, z2, x, y, z):
+        """
+        Calculate the effect of the cell with a physical property prop.
+        """
+        raise NotImplementedError("Method 'calc_col' was not implemented")        
+
+    def residuals(self, mesh, estimate, neighbor):
+        """
+        Calculate the residuals vector due to adding a neighbor to the estimate.
+
+        Parameters:
+        * mesh
+            A 3D mesh. See :mod:`fatiando.mesher.volume`
+        * estimate
+            Dictionary with the physical properties of the current estimate
+            without the neighbor. *IGNORED*
+        * neighbor
+            [n, props]
+            n is the index of the neighbor in the mesh.
+            props is a dictionary with the physical properties of the neighbor
+
+        Returns:
+        * array
+            Array with the residuals
+            
+        """
+        n, props = neighbor
+        if self.prop not in props:
+            return self.res
+        prop = float(props[self.prop])
+        if n not in self.effect:
+            c = mesh[n]
+            self.effect[n] = self.calc_effect(prop, c['x1'], c['x2'], c['y1'],
+                c['y2'], c['z1'], c['z2'], self.x, self.y, self.z)  
+        return self.res - self.effect[n]
+        
+    def cleanup(self, neighbor):
+        """
+        Delete unnecessary things after neighbor has been added to the estimate.
+
+        Parameters:
+        * neighbor
+            [n, props]
+            n is the index of the neighbor in the mesh.
+            props is a dictionary with the physical properties of the neighbor
+            
+        """
+        self.res = self.res - self.effect[neighbor[0]]
+        del self.effect[neighbor[0]]
+
+    def predicted(self):
+        """
+        The predicted data at the end of an inversion process.
+        Calculated by:
+            pred = obs - residuals
+
+        Returns:
+        * array
+            Array of predicted data.
+            
+        """
+        return self.obs - self.res
+        
+class GzModule(PotentialModule):
+    """
+    Data module for the vertical component of the gravitational attraction (gz).
+
+    Remember: the coordinate system is x->North, y->East, and z->Down
+
+    Parameters:
+    * x, y, z
+        Arrays with the x, y, and z coordinates of the data points.
+    * obs
+        Array with the observed values of the gz component of the gravity field.
+    * norm
+        Order of the norm of the residual vector to use (Default: 2). Can be:
+        * 1 -> l1 norm
+        * 2 -> l2 norm
+        * etc
+    
+    """
+
+    def __init__(self, x, y, z, obs, norm=2):
+        PotentialModule.__init__(self, x, y, z, obs, norm)
+        self.prop = 'density'
+        self.calc_effect = potential._prism.prism_gz
+        
+def loadseeds(fname, prop):
+    """
+    Load a set of seed locations and physical properties from a file.
+
+    The file should have 4 columns: x, y, z, value
+    x, y, and z are the coordinates where the seed should be put. value is value
+    of the physical property associated with the seed.
+
+    Remember: the coordinate system is x->North, y->East, and z->Down
+
+    Parameters:
+    * fname
+        Open file object or filename string
+    * prop
+        String with the name of the physical property. Ex: density
+
+    Returns:
+    * list
+        A list with the position and physical property of the seeds, as required
+        by :func:`fatiando.inversion.harvester.sow`::        
+            [((x1,y1,z1), {prop:value1}), ((x2,y2,z2), {prop:value2}), ...] 
+    
+    """
+    return [((x, y, z), {prop:v}) for x, y, z, v  in numpy.loadtxt(fname)]
+
+def sow(mesh, rawseeds):
+    """ 
+    Find the index of the seeds in the mesh given their (x,y,z) location.
+
+    The output of this function should be passed to
+    :func:`fatiando.inversion.harvester.harvest`
+
+    Parameters:
+    * mesh
+        A 3D mesh. See :mod:`fatiando.mesher.volume`
+    * rawseeds
+        A list with the position and physical property of each seed::        
+            [((x1,y1,z1), {'density':v1}), ((x2,y2,z2), {'density':v2}), ...] 
+
+    Returns:
+    * list
+        A list the index of each seed in the mesh and their physical properties,
+        as required by :func:`fatiando.inversion.harvester.harvest`
+        
+    """
+    seeds = []
+    append = seeds.append
+    for point, props in rawseeds:
+        x, y, z = point
+        for s, cell in enumerate(mesh):
+            if (x >= cell['x1'] and x <= cell['x2'] and y >= cell['y1'] and  
+                y <= cell['y2'] and z >= cell['z1'] and z <= cell['z2']):
+                append((s, props))
+                break
+    # Search for duplicates
+    duplicates = []
+    for i in xrange(len(seeds)):
+        si, pi = seeds[i]
+        for j in xrange(i+1, len(seeds)):
+            sj, pj = seeds[j]
+            if si == sj and True in [p in pi for p in pj]:
+                duplicates.append((i,j))
+    if duplicates:
+        guilty = ', '.join(['%d and %d' % (i, j) for i, j in duplicates])
+        msg1 = "Can't have seeds with same location and physical properties!"
+        msg2 = "Guilty seeds: %s" % (guilty)
+        raise ValueError, ' '.join([msg1, msg2])    
+    return seeds
 
 def grow(seeds, mesh, dmods, compact=0, thresh=0.0001):
     """
@@ -56,6 +289,7 @@ def harvest(seeds, mesh, dmods, compact=0, thresh=0.0001):
     Perform all accretions. Return mesh with special props in it.
     special props don't store 0s, return 0 if try to access element not in dict
     """
-    pass
+    
+    return estimate
 
 
