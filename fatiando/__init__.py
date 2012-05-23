e9608ca03868fb004bf273778d015b54598f1d4c
Leonardo Uieda <leouieda@gmail.com>
2012-05-23 12:59:26 -0300

    When importing fatiando, modules and packages get nicknames

--- fatiando/__init__.py
+++ fatiando/__init__.py
@@ -3,33 +3,41 @@
 
 Tools for geophysical modeling and inversion.
 
-Contains utilities for gridding, meshing, and visualization:
+Contains utilities for gridding, meshing, and visualization.
 
-* :mod:`fatiando.mesher`
-    Mesh generation and definition of geometric elements
-* :mod:`fatiando.gridder`
-    Grid generation and operations (like interpolation)
-* :mod:`fatiando.vis`
-    Plotting utilities for maps (using matplotlib) and 3D (using mayavi)
-* :mod:`fatiando.ui`
-    User interfaces, like map picking and interactive drawing
-* :mod:`fatiando.utils`
-    Miscelaneous utilities, like mathematical functions, unit conversion, etc
-* :mod:`fatiando.logger`
-    An interface to Pythons :mod:`logging` module for easy printing to log files
+Some modules have "nicknames" for easier access when importing ``fatiando``
+directly. To import each module separately, use the full names:
+
+* :mod:`msh <fatiando.mesher>`:
+  Mesh generation and definition of geometric elements
+* :mod:`grd <fatiando.gridder>`:
+  Grid generation and operations (like interpolation)
+* :mod:`vis <fatiando.vis>`:
+  Plotting utilities for maps (using matplotlib) and 3D (using mayavi)
+* :mod:`ui <fatiando.ui>`:
+  User interfaces, like map picking and interactive drawing
+* :mod:`utils <fatiando.utils>`:
+  Miscelaneous utilities, like mathematical functions, unit conversion, etc
+* :mod:`log <fatiando.logger>`:
+  An interface to Pythons :mod:`logging` module for easy printing to log files
 
 Modules for specific geophysical methods are divided into subpackages:
 
-* :mod:`fatiando.potential`
-    Potential fields
-* :mod:`fatiando.seismic`
-    Seismic method and seismology
-* :mod:`fatiando.heat`
-    Geothermics
+* :mod:`pot <fatiando.potential>`:
+  Potential fields
+* :mod:`seis <fatiando.seismic>`:
+  Seismic method and seismology
+* :mod:`heat <fatiando.heat>`:
+  Geothermics
+
+See the documentation for each module to find out more about what they do and
+how to use them.
 
 Also included is the :mod:`fatiando.inversion` package with utilities for
 implementing inverse problems. There you'll find common regularizing functions,
-linear inverse problem solvers, and non-linear gradient solvers.
+linear inverse problem solvers, and non-linear gradient solvers. This package
+is generaly only used from inside Fatiando itself, not when using Fatiando in
+scripts.
 
 ----
 
@@ -39,6 +47,13 @@ version = '0.1.dev'
 
 # Import all the modules and subpackages so that they are accessible just by
 # importing fatiando
-from fatiando import logger, gridder, utils
-from fatiando import heat, mesher, potential, seismic, ui, vis
+from fatiando import logger as log
+from fatiando import gridder as grd
+from fatiando import utils
+from fatiando import heat
+from fatiando import mesher as msh
+from fatiando import potential as pot
+from fatiando import seismic as seis
+from fatiando import ui
+from fatiando import vis
 from fatiando import inversion
