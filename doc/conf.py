05d119a918f02c862e9953d6e15e51548119f403
Leonardo Uieda <leouieda@gmail.com>
2012-05-28 11:24:35 -0300

    Better latex logo with text

--- doc/conf.py
+++ doc/conf.py
@@ -219,7 +219,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'fatiando-%s.tex' % (version), u'Fatiando a Terra',
+  ('index', 'fatiando-%s.tex' % (version), u'User and developer guide',
    u'Leonardo Uieda', 'manual'),
 ]
 
