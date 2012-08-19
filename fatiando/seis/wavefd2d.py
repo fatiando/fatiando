4c8494d9271489458f8d4c4be1e2aca8e716636e
Leonardo Uieda <leouieda@gmail.com>
2012-08-19 10:24:45 -0300

    Added initial skeleton for seis.wavefd2d

---
+++ fatiando/seis/wavefd2d.py
@@ -0,0 +1,27 @@
+"""
+Finite difference solution of the 2D wave equation for isotropic media.
+
+Simulates both elastic and acoustic waves:
+
+* :func:`~fatiando.seis.wavefd2d.elastic_psv`: Simulates the coupled P and SV
+  elastic waves
+* :func:`~fatiando.seis.wavefd2d.elastic_sh`: Simlates SH elastic waves
+
+
+**Theory**
+
+
+
+----
+"""
+import numpy
+
+import fatiando.log
+
+log = fatiando.log.dummy('fatiando.seis.wavefd2d')
+
+
+def elastic_sh():
+    """
+    """
+    pass
