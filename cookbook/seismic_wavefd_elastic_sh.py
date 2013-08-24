f3194f909244d38223002e77ec3cba1031068474
Leonardo Uieda <leouieda@gmail.com>
2013-08-23 21:18:46 -0300

    fixed the wavefd recipes

--- cookbook/seismic_wavefd_elastic_sh.py
+++ cookbook/seismic_wavefd_elastic_sh.py
@@ -3,55 +3,57 @@ Seismic: 2D finite difference simulation of elastic SH wave propagation
 """
 import numpy as np
 from matplotlib import animation
-from fatiando import seismic, gridder
+from fatiando import gridder
+from fatiando.seismic import wavefd
 from fatiando.vis import mpl
 
-# Make a wave source from a mexican hat wavelet
-sources = [seismic.wavefd.MexHatSource(25, 25, 100, 0.5, delay=1.5)]
 # Set the parameters of the finite difference grid
-shape = (100, 100)
-spacing = (500, 500)
-area = (0, spacing[1]*shape[1], 0, spacing[0]*shape[0])
+shape = (150, 150)
+area = [0, 60000, 0, 60000]
 # Make a density and S wave velocity model
-dens = 2700*np.ones(shape)
-mu = 2700*(3000**2)*np.ones(shape)
+density = 2400*np.ones(shape)
+velocity = 3700
+mu = wavefd.lame_mu(velocity, density)
 
-# Get the iterator. This part only generates an iterator object. The actual
-# computations take place at each iteration in the for loop below
-dt = 0.05
-maxit = 1500
-timesteps = seismic.wavefd.elastic_sh(spacing, shape, mu, dens, dt, maxit,
-    sources, padding=50, taper=0.005)
+# Make a wave source from a mexican hat wavelet
+sources = [wavefd.MexHatSource(30000, 15000, area, shape, 100, 1, delay=2)]
+
+# Get the iterator for the simulation
+dt = 0.065
+duration = 20
+maxit = int(duration/dt)
+stations = [[50000, 0]] # x, z coordinate of the seismometer
+snapshot = int(0.5/dt) # Plot a snapshot of the simulation every 0.5 seconds
+simulation = wavefd.elastic_sh(mu, density, area, dt, maxit, sources, stations,
+                               snapshot, padding=50, taper=0.01)
 
 # This part makes an animation using matplotlibs animation API
-vmin, vmax = -1*10**(-5), 1*10**(-5)
 fig = mpl.figure(figsize=(14, 5))
-mpl.subplot(1, 2, 1)
-x, z = gridder.regular(area, shape)
+ax = mpl.subplot(1, 2, 2)
+mpl.title('Wavefield')
 # Start with everything zero and grab the plot so that it can be updated later
-initial = timesteps.next()
-wavefield = mpl.imshow(initial, vmin=vmin, vmax=vmax)
-mpl.xlabel("x (km)")
-mpl.ylabel("z (km)")
-mpl.set_area([0, initial.shape[1], initial.shape[0], 0])
-mpl.subplot(1, 2, 2)
-energy, = mpl.plot([],[],'-k')
-mpl.xlim(0, maxit)
+wavefield_plt = mpl.imshow(np.zeros(shape), extent=area, vmin=-10**(-5),
+    vmax=10**(-5), cmap=mpl.cm.gray_r)
+mpl.points(stations, '^b')
+mpl.xlim(area[:2])
+mpl.ylim(area[2:][::-1])
+mpl.xlabel('x (km)')
+mpl.ylabel('z (km)')
+mpl.subplot(1, 2, 1)
+seismogram_plt, = mpl.plot([],[],'-k')
+mpl.xlim(0, duration)
 mpl.ylim(-10**(-4), 10**(-4))
-values = []
-# This function updates the plot every few timesteps
-steps_per_frame = 10
+mpl.xlabel('time (s)')
+mpl.ylabel('Amplitude')
+times = np.linspace(0, duration, maxit)
+# Update the plot everytime the simulation yields
 def animate(i):
-    for t, u in enumerate(timesteps):
-        values.append(u[0, u.shape[1]/2])
-        if t == steps_per_frame - 1:
-            mpl.title('time: %0.1f s' % (i*steps_per_frame*dt))
-            wavefield.set_array(u)
-            energy.set_data(range(len(values)),values)
-            break
-    return wavefield, energy
-anim = animation.FuncAnimation(fig, animate,
-    frames=maxit/steps_per_frame, interval=10, blit=False)
-#anim.save('sh_wave.mp4', fps=10)
+    # Grab the iteration number, displacment panel and seismograms
+    t, u, seismograms = simulation.next()
+    mpl.title('time: %0.1f s' % (times[t]))
+    wavefield_plt.set_array(u[::-1]) # Revert the z axis so that 0 is top
+    seismogram_plt.set_data(times[:t + 1], seismograms[0][:t + 1])
+    return wavefield_plt, seismogram_plt
+anim = animation.FuncAnimation(fig, animate, frames=maxit/snapshot, interval=1)
+#anim.save('sh_wave.mp4', fps=10, dpi=200, bitrate=4000)
 mpl.show()
-
