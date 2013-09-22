bfedffc4d2f1f461a82a06a806dc0c770f14c6f3
Leonardo Uieda <leouieda@gmail.com>
2013-09-22 15:17:13 -0300

    Removed parallel fwd modeling from srtomo recipes
    Don't need it because of new speedups.
    It's not longer a bottleneck as the serial version is 300x faster than
    the old parallel version.

--- cookbook/seismic_srtomo_sharp.py
+++ cookbook/seismic_srtomo_sharp.py
@@ -23,7 +23,7 @@ seed = 0 # Set the random seed so that points are the same everythime
 src_loc = utils.random_points(area, 80, seed=seed)
 rec_loc = utils.circular_points(area, 30, random=True, seed=seed)
 srcs, recs = utils.connect_points(src_loc, rec_loc)
-tts = seismic.ttime2d.straight(model, 'vp', srcs, recs, par=True)
+tts = seismic.ttime2d.straight(model, 'vp', srcs, recs)
 tts, error = utils.contaminate(tts, 0.01, percent=True, return_stddev=True)
 # Make the mesh
 mesh = mesher.SquareMesh(area, shape)
