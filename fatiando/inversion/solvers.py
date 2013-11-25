ec415e4a67b6304d214039d65de6f4fc77cf6591
Leonardo Uieda <leouieda@gmail.com>
2013-11-25 14:03:28 -0200

    Newton's method in inversion.solvers
    Implemented Newton's method in solvers. The stats of the gradient
    descent are recorded in an optional stats dict.

--- fatiando/inversion/solvers.py
+++ fatiando/inversion/solvers.py
@@ -44,3 +44,78 @@ def linear(hessian, gradient, precondition=True):
         gradient = safe_dot(precond, gradient)
     p = safe_solve(hessian, -gradient)
     return p
+
+def newton(initial, hessian, gradient, value, maxit=30, tol=10**-5,
+           precontition=True, stats=None):
+    """
+    Minimize an objective function using Newton's method.
+
+    Newton's method searches for the minimum of an objective function
+    :math:`\phi(\bar{p})` by successively incrementing the initial estimate.
+    The increment is the solution of the linear system
+
+    .. math::
+
+        \bar{\bar{H}}(\bar{p}^k) \bar{\Delta p}^k = -\bar{g}(\bar{p}^k)
+
+    where :math:`\bar{\bar{H}}` is the Hessian matrix of :math:`\phi` and
+    :math:`\bar{g}` is the gradient vector of :math:`\phi`. Both are evaluated
+    at the previous estimate :math:`\bar{p}^k`.
+
+
+    Parameters:
+
+    * initial : 1d-array
+        The initial estimate for the gradient descent.
+    * hessian : function
+        A function that returns the Hessian matrix of the objective function
+        when given a parameter vector.
+    * gradient : function
+        A function that returns the gradient vector of the objective function
+        when given a parameter vector.
+    * value : function
+        A function that returns the value of the objective function evaluated
+        at a given parameter vector.
+    * maxit : int
+        The maximum number of iterations allowed.
+    * tol : float
+        The convergence criterion. The lower it is, the more steps are
+        permitted.
+    * precondition : True or False
+        If True, will use Jacobi preconditioning.
+    * stats : None or a dictionary
+        If a dict, will fill the dictionary with information about the
+        optimization procedure.
+
+    Yields:
+
+    * estimate : 1d-array
+        The estimated parameter vector at the current iteration.
+
+    """
+    p = initial.astype(numpy.float)
+    misfit = value(p)
+    if stats is not None:
+        stats['method'] = 'Newton'
+        stats['iterations'] = 0
+        stats['misfit/iteration'] = [misfit]
+        stats['preconditioned'] = precondition
+    yield p
+    for iteration in xrange(maxit):
+        hess = hessian(p)
+        grad = gradient(p)
+        if precondition:
+            diag = numpy.abs(safe_diagonal(hess))
+            diag[diag < 10**-10] = 10**-10
+            precond = scipy.sparse.diags(1./diag, 0).tocsr()
+            hess = safe_dot(precond, hess)
+            grad = safe_dot(precond, grad)
+        p = p + safe_solve(hess, -grad)
+        newmisfit = self.value(p)
+        if newmisfit > misfit or abs((newmisfit - misfit)/misfit) < tol:
+            break
+        misfit = newmisfit
+        if stats is not None:
+            stats['iterations'] += 1
+            stats['misfit/iteration'].append(misfit)
+        yield p
