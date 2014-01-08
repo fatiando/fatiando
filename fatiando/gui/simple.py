a012948ce63c2f4ba8ea29f9af2228e5880d7aec
Leonardo Uieda <leouieda@gmail.com>
2014-01-08 12:18:34 -0200

    Switched to relative imports in gui.simple
    Also removed module import from gui/__init__.py

--- fatiando/gui/simple.py
+++ fatiando/gui/simple.py
@@ -19,10 +19,10 @@ import bisect
 import numpy
 from matplotlib import pyplot, widgets
 
-from fatiando.gravmag import talwani
-from fatiando.mesher import Polygon
-from fatiando import utils
-import fatiando.seismic
+from .. import utils
+from ..gravmag import talwani
+from ..mesher import Polygon
+from ..seismic import profile
 
 
 class Moulder():
@@ -511,7 +511,7 @@ class Lasagne():
         # Initialize the data
         self.error = 0.
         self.velocity = vmin*numpy.ones_like(thickness)
-        self.predtts = fatiando.seismic.profile.vertical(thickness, self.velocity,
+        self.predtts = profile.vertical(thickness, self.velocity,
                                                       zp)
         self.layers = [sum(thickness[:i]) for i in xrange(len(thickness) + 1)]
         self.predplot, = self.dcanvas.plot(self.predtts, zp, '-r', linewidth=2)
@@ -552,7 +552,7 @@ class Lasagne():
 
     def update(self):
         self.predtts = utils.contaminate(
-            fatiando.seismic.profile.vertical(self.thickness, self.velocity,
+            profile.vertical(self.thickness, self.velocity,
                 self.zp),
             self.error, percent=True)
         self.predplot.set_data(self.predtts, self.zp)
