99476f0317f802c97a8bec7d2f5c9dcc060de584
Leonardo Uieda <leouieda@gmail.com>
2014-01-11 14:00:45 -0200

    tensor.center_of_mass no longer returns sigma
    sigma was a (bad) measure of uncertainty and wasn't actually used
    anywhere other than internally by the function.
    
    Also improved the doctest by using a sphere model (easier to know the
    true center of mass)

--- cookbook/gravmag_tensor_centerofmass.py
+++ cookbook/gravmag_tensor_centerofmass.py
@@ -3,19 +3,20 @@ GravMag: Center of mass estimation using the first eigenvector of the gravity
 gradient tensor (simple model)
 """
 from fatiando.vis import mpl, myv
-from fatiando import mesher, gridder, utils, gravmag
+from fatiando import mesher, gridder, utils
+from fatiando.gravmag import prism, tensor
 
 # Generate some synthetic data
-prisms = [mesher.Prism(-1000,1000,-1000,1000,1000,3000,{'density':1000})]
+model = [mesher.Prism(-1000,1000,-1000,1000,1000,3000,{'density':1000})]
 shape = (100, 100)
 xp, yp, zp = gridder.regular((-5000, 5000, -5000, 5000), shape, z=-150)
 noise = 2
-tensor = [utils.contaminate(gravmag.prism.gxx(xp, yp, zp, prisms), noise),
-          utils.contaminate(gravmag.prism.gxy(xp, yp, zp, prisms), noise),
-          utils.contaminate(gravmag.prism.gxz(xp, yp, zp, prisms), noise),
-          utils.contaminate(gravmag.prism.gyy(xp, yp, zp, prisms), noise),
-          utils.contaminate(gravmag.prism.gyz(xp, yp, zp, prisms), noise),
-          utils.contaminate(gravmag.prism.gzz(xp, yp, zp, prisms), noise)]
+data = [utils.contaminate(prism.gxx(xp, yp, zp, model), noise),
+        utils.contaminate(prism.gxy(xp, yp, zp, model), noise),
+        utils.contaminate(prism.gxz(xp, yp, zp, model), noise),
+        utils.contaminate(prism.gyy(xp, yp, zp, model), noise),
+        utils.contaminate(prism.gyz(xp, yp, zp, model), noise),
+        utils.contaminate(prism.gzz(xp, yp, zp, model), noise)]
 # Plot the data
 titles = ['gxx', 'gxy', 'gxz', 'gyy', 'gyz', 'gzz']
 mpl.figure()
@@ -23,20 +24,19 @@ for i, title in enumerate(titles):
     mpl.subplot(3, 2, i + 1)
     mpl.title(title)
     mpl.axis('scaled')
-    levels = mpl.contourf(yp, xp, tensor[i], shape, 10)
-    mpl.contour(yp, xp, tensor[i], shape, levels)
+    levels = mpl.contourf(yp, xp, data[i], shape, 10)
+    mpl.contour(yp, xp, data[i], shape, levels)
     mpl.m2km()
 mpl.show()
 # Get the eigenvectors from the tensor data
-eigenvals, eigenvecs = gravmag.tensor.eigen(tensor)
+eigenvals, eigenvecs = tensor.eigen(data)
 # Use the first eigenvector to estimate the center of mass
-cm, sigma = gravmag.tensor.center_of_mass(xp, yp, zp, eigenvecs[0])
-# Sigma is the RMS error
-print "Sigma = %g" % (sigma)
+cm = tensor.center_of_mass(xp, yp, zp, eigenvecs[0])
+
 # Plot the prism and the estimated center of mass
 myv.figure()
 myv.points([cm], size=200.)
-myv.prisms(prisms, prop='density', opacity=0.5)
+myv.prisms(model, prop='density', opacity=0.5)
 axes = myv.axes(
     myv.outline(extent=[-5000, 5000, -5000, 5000, 0, 5000]))
 myv.wall_bottom(axes.axes.bounds, opacity=0.2)
