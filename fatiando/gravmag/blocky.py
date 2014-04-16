65695d4575bd59c4e3570c80f43cd8acda828d9e
Leonardo Uieda <leouieda@gmail.com>
2014-04-15 21:01:49 -0300

    Initial layout of blocky inversion
    Made the Gravity class for general blocky inversion and its harvest
    method (will behave like linear, levmarq, etc). Most of the class is
    done. What is left is the _get_neighbors method. Copied the seed
    classes and sow function from harvester.

---
+++ fatiando/gravmag/blocky.py
@@ -0,0 +1,350 @@
+"""
+3D linear inversion using blocky models (prisms and tesseroids).
+
+**Planting inversion**
+
+>>> import numpy as np
+>>> from fatiando import gridder, utils
+>>> from fatiando.gravmag import prism
+>>> from fatiando.mesher import Prism, PrismMesh
+>>> # Create a model
+>>> model = [Prism(4000, 6000, 4000, 6000, 1000, 4000, {'density':500})]
+>>> # and generate noisy synthetic data
+>>> shape = (25, 25)
+>>> bounds = [0, 10000, 0, 10000, 0, 5000]
+>>> area = bounds[0:4]
+>>> x, y, z = gridder.regular(area, shape, z=-1)
+>>> gz = utils.contaminate(prism.gz(x, y, z, model), 0.1, seed=0)
+>>> # Setup the inversion by creating a mesh and seeds
+>>> mesh = PrismMesh(bounds, (5, 10, 10))
+>>> seeds = sow([[5000, 5000, 2000, {'density':500}]], mesh)
+>>> # Run the inversion
+>>> solver = Gravity(x, y, z, gz, mesh).config(
+...     'planting', seeds=seeds, compactness=1, threshold=0.001).fit()
+>>> # Lets print the solution
+>>> for top, layer in zip(mesh.get_zs(), solver.estimate_.reshape(mesh.shape)):
+...     print("top: {} m".format(top))
+...     print(layer)
+top: 0.0 m
+[[ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
+ [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
+ [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
+ [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
+ [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
+ [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
+ [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
+ [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
+ [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
+ [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]]
+top: 1000.0 m
+[[   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.  500.  500.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.  500.  500.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]]
+top: 2000.0 m
+[[   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.  500.  500.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.  500.  500.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]]
+top: 3000.0 m
+[[   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.  500.  500.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.  500.  500.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]
+ [   0.    0.    0.    0.    0.    0.    0.    0.    0.    0.]]
+top: 4000.0 m
+[[ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
+ [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
+ [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
+ [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
+ [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
+ [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
+ [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
+ [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
+ [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
+ [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]]
+
+
+"""
+from __future__ import division
+import bisect
+from math import sqrt
+
+import numpy
+
+from . import prism as prism_engine
+from . import tesseroid as tesseroid_engine
+from ..utils import safe_dot
+from ..inversion.base import Misfit
+from ..mesher import Prism, Tesseroid
+
+class Gravity(Misfit):
+    def __init__(self, x, y, z, data, mesh, field='gz'):
+        super(Gravity, self).__init__(data=data,
+            positional=dict(x=x, y=y, z=z),
+            model={'mesh': mesh},
+            nparams=mesh.size,
+            islinear=True)
+        self.kernel = None
+        if mesh.celltype == 'prism':
+            self.kernel = getattr(prism_engine, field)
+        elif mesh.celltype == 'tesseroid':
+            self.kernel = getattr(tesseroid_engine, field)
+        self.dnorm = numpy.linalg.norm(data)
+        self.prop = 'density'
+        self._effects = {}
+
+    def _get_predicted(self, p):
+        if self._cache['jacobian']['array'] is None:
+            x = self.positional['x']
+            y = self.positional['y']
+            z = self.positional['z']
+            mesh = self.model['mesh']
+            return numpy.sum(self.kernel(x, y, z, [c], dens=p[i])
+                             for i, c in enumerate(mesh) if p[i] != 0)
+        else:
+            return safe_dot(self.jacobian(None), p)
+
+    def _get_jacobian(self, p):
+        x = self.positional['x']
+        y = self.positional['y']
+        z = self.positional['z']
+        mesh = self.model['mesh']
+        jac = numpy.empty((self.ndata, self.nparams), dtype=float)
+        for i, c in enumerate(mesh):
+            jac[:, i] = self.kernel(x, y, z, [c], dens=1)
+        return jac
+
+    def shape_of_anomaly(self, p, increment=None):
+        if self._parents is None:
+            value = self._get_shape_of_anomaly(p, increment)
+        else:
+            if self._scale is None:
+                obj1, obj2 = self._parents
+                value = (obj1.shape_of_anomaly(p, increment)
+                         + obj2.shape_of_anomaly(p, increment))
+            else:
+                assert len(self._parents) == 1, \
+                    'Result of multiplying Objective produces > one parent.'
+                obj = self._parents[0]
+                value = self._scale*obj.shape_of_anomaly(p, increment)
+        return value
+
+    def _get_shape_of_anomaly(self, p, increment=None):
+        predicted = self.predicted(p)
+        if increment is not None:
+            prop = increment.props[self.prop]
+            predicted += prop*self._get_effect(increment.i)
+        alpha = numpy.sum(self.data*predicted)/self.dnorm**2
+        return numpy.linalg.norm(alpha*self.data - predicted)
+
+    def _get_value(self, p, **kwargs):
+        predicted = self.predicted(p)
+        increment = kwargs.get('increment')
+        if increment is not None:
+            prop = increment.props[self.prop]
+            predicted += prop*self._get_effect(increment.i)
+        if self.weights is None:
+            value = numpy.linalg.norm(self.data - predicted)
+        else:
+            value = sqrt(numpy.sum(self.weights*((self.data - predicted)**2)))
+        return value/self.dnorm
+
+    def _get_effect(self, index):
+        if index not in self._effects:
+            x = self.positional['x']
+            y = self.positional['y']
+            z = self.positional['z']
+            mesh = self.model['mesh']
+            self._effects[index] = self.kernel(x, y, z, [mesh[index]], dens=1)
+        return self._effects[index]
+
+    def harvest(self, seeds, threshold, compactness):
+        mesh = self.model['mesh']
+        seeds = [s for s in seeds if self.prop in s.props]
+        nseeds = len(seeds)
+        estimate = numpy.zeros(self.nparams)
+        for seed in seeds:
+            estimate[seed.i] = seed.props[self.prop]
+        neighbors = []
+        for seed in seeds:
+            neighbors.append(self._get_neighbors(seed, neighbors, estimate))
+        misfit = self.value(estimate)
+        goal = self.shape_of_anomaly(estimate)
+        compact = 0
+        # Weight the regularizing function by the mean extent of the mesh
+        mu = compactness*1/(sum(mesh.shape)/3)
+        accretions = 0
+        for iteration in xrange(mesh.size - nseeds):
+            grew = False
+            for s in xrange(nseeds):
+                best = self._grow(neighbors[s], misfit, goal, compact, mu,
+                                  threshold, estimate)
+                if best is not None:
+                    goal = best['goal']
+                    misfit = best['misfit']
+                    compact = best['compact']
+                    estimate = self._update_cache(best['neighbor'], estimate)
+                    neighbors[s].pop(best['neighbor'].i)
+                    neighbors[s].update(self._get_neighbors(best['neighbor'],
+                                        neighbors, estimate))
+                    grew = True
+                    accretions += 1
+            if not grew:
+                break
+
+    def _get_neightbors(self, seed, neighbors, estimate):
+        pass
+
+    def _grow(self, neighbors, misfit, goal, compact, mu, threshold, estimate):
+        best = None
+        for n in neighbors:
+            neighbor = neighbors[n]
+            newmisfit = self.value(estimate, increment=neighbor)
+            if (newmisfit >= misfit
+                or abs(newmisfit - misfit)/misfit < threshold):
+                continue
+            newcompact = compact + mu*neighbor.distance
+            newgoal = (self.shape_of_anomaly(estimate, increment=neighbor)
+                       + newcompact)
+            if best is None or newgoal < best['goal']:
+                best = dict(neighbor=neighbor, goal=newgoal, misfit=newmisfit,
+                            compact=newcompact)
+        return best
+
+    def _update_cache(self, neighbor, estimate):
+        predicted = self.predicted(None)
+        prop = neighbor.props[self.prop]
+        predicted += prop*self._get_effect(neighbor.i)
+        estimate[neighbor.i] = prop
+        hash = self.hasher(estimate)
+        self._cache['predicted']['hash'] = hash
+        self._cache['predicted']['array'] = predicted
+        return estimate
+
+class PrismSeed(Prism):
+    """
+    A seed that is a right rectangular prism.
+    """
+
+    def __init__(self, i, location, prism, props):
+        Prism.__init__(self, prism.x1, prism.x2, prism.y1, prism.y2, prism.z1,
+            prism.z2, props=props)
+        self.i = i
+        self.seed = i
+        self.x, self.y, self.z = location
+
+class TesseroidSeed(Tesseroid):
+    """
+    A seed that is a tesseroid (spherical prism).
+    """
+
+    def __init__(self, i, location, tess, props):
+        Tesseroid.__init__(self, tess.w, tess.e, tess.s, tess.n, tess.top,
+            tess.bottom, props=props)
+        self.i = i
+        self.seed = i
+        self.x, self.y, self.z = location
+
+class Neighbor(object):
+    """
+    A neighbor.
+    """
+
+    def __init__(self, i, props, seed, distance):
+        self.i = i
+        self.props = props
+        self.seed = seed
+        self.distance = distance
+
+def sow(locations, mesh):
+    """
+    Create the seeds given a list of (x,y,z) coordinates and physical
+    properties.
+
+    Removes seeds that would fall on the same location with overlapping
+    physical properties.
+
+    Parameters:
+
+    * locations : list
+        The locations and physical properties of the seeds. Should be a list
+        like::
+
+            [
+                [x1, y1, z1, {"density":dens1}],
+                [x2, y2, z2, {"density":dens2, "magnetization":mag2}],
+                [x3, y3, z3, {"magnetization":mag3, "inclination":inc3,
+                              "declination":dec3}],
+                ...
+            ]
+
+    * mesh : :class:`fatiando.mesher.PrismMesh`
+        The mesh that will be used in the inversion.
+
+    Returns:
+
+    * seeds : list of seeds
+        The seeds that can be passed to
+        :func:`~fatiando.gravmag.harvester.harvest`
+
+    """
+    seeds = []
+    if mesh.celltype == Tesseroid:
+        seedtype = TesseroidSeed
+    elif mesh.celltype == Prism:
+        seedtype = PrismSeed
+    for x, y, z, props in locations:
+        index = _find_index((x, y, z), mesh)
+        if index is None:
+            raise ValueError(
+                "Couldn't find seed at location (%g,%g,%g)" % (x, y, z))
+        # Check for duplicates
+        if index not in (s.i for s in seeds):
+            seeds.append(seedtype(index, (x, y, z), mesh[index], props))
+    return seeds
+
+def _find_index(point, mesh):
+    """
+    Find the index of the cell that has point inside it.
+    """
+    x1, x2, y1, y2, z1, z2 = mesh.bounds
+    nz, ny, nx = mesh.shape
+    xs = mesh.get_xs()
+    ys = mesh.get_ys()
+    zs = mesh.get_zs()
+    x, y, z = point
+    if (x <= x2 and x >= x1 and y <= y2 and y >= y1 and
+        ((z <= z2 and z >= z1 and mesh.zdown) or
+         (z >= z2 and z <= z1 and not mesh.zdown))):
+        if mesh.zdown:
+            # -1 because bisect gives the index z would have. I want to know
+            # what index z comes after
+            k = bisect.bisect_left(zs, z) - 1
+        else:
+            # If z is not positive downward, zs will not be sorted
+            k = len(zs) - bisect.bisect_left(zs[::-1], z)
+        j = bisect.bisect_left(ys, y) - 1
+        i = bisect.bisect_left(xs, x) - 1
+        seed = i + j*nx + k*nx*ny
+        # Check if the cell is not masked (topography)
+        if mesh[seed] is not None:
+            return seed
+    return None
