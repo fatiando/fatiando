4eb556f5ce47b923fa9a18be5cba8aa96aa2dfcf
Leonardo Uieda <leouieda@gmail.com>
2014-07-21 15:05:50 -0300

    Docstring for gravmag.tesseroid

--- fatiando/gravmag/tesseroid.py
+++ fatiando/gravmag/tesseroid.py
@@ -1,5 +1,141 @@
-"""
-Calculates the potential fields of a tesseroid.
+r"""
+Calculates the potential fields of a tesseroid (spherical prism).
+
+.. admonition:: Coordinate systems
+
+    The gravitational attraction
+    and gravity gradient tensor
+    are calculated with respect to
+    the local coordinate system of the computation point.
+    This system has **x -> North**, **y -> East**, **z -> up**
+    (radial direction).
+
+**Gravity**
+
+.. warning:: The :math:`g_z` component is an **exception** to this.
+    In order to conform with the regular convention
+    of z-axis pointing toward the center of the Earth,
+    **this component only** is calculated with **z -> Down**.
+    This way, gravity anomalies of
+    tesseroids with positive density
+    are positive, not negative.
+
+Functions:
+:func:`~fatiando.gravmag.prism.potential`,
+:func:`~fatiando.gravmag.prism.gx`,
+:func:`~fatiando.gravmag.prism.gy`,
+:func:`~fatiando.gravmag.prism.gz`,
+:func:`~fatiando.gravmag.prism.gxx`,
+:func:`~fatiando.gravmag.prism.gxy`,
+:func:`~fatiando.gravmag.prism.gxz`,
+:func:`~fatiando.gravmag.prism.gyy`,
+:func:`~fatiando.gravmag.prism.gyz`,
+:func:`~fatiando.gravmag.prism.gzz`
+
+The gravitational fields are calculated using the formula of Grombein et al.
+(2013):
+
+.. math::
+    V(r,\phi,\lambda) = G \rho
+        \displaystyle\int_{\lambda_1}^{\lambda_2}
+        \displaystyle\int_{\phi_1}^{\phi_2}
+        \displaystyle\int_{r_1}^{r_2}
+        \frac{1}{\ell} \kappa \ d r' d \phi' d \lambda'
+
+.. math::
+    g_{\alpha}(r,\phi,\lambda) = G \rho
+        \displaystyle\int_{\lambda_1}^{\lambda_2}
+        \displaystyle\int_{\phi_1}^{\phi_2} \displaystyle\int_{r_1}^{r_2}
+        \frac{\Delta_{\alpha}}{\ell^3} \kappa \ d r' d \phi' d \lambda'
+        \ \ \alpha \in \{x,y,z\}
+
+.. math::
+    g_{\alpha\beta}(r,\phi,\lambda) = G \rho
+        \displaystyle\int_{\lambda_1}^{\lambda_2}
+        \displaystyle\int_{\phi_1}^{\phi_2} \displaystyle\int_{r_1}^{r_2}
+        I_{\alpha\beta}({r'}, {\phi'}, {\lambda'} )
+        \ d r' d \phi' d \lambda'
+        \ \ \alpha,\beta \in \{x,y,z\}
+
+.. math::
+    I_{\alpha\beta}({r'}, {\phi'}, {\lambda'}) =
+        \left(
+            \frac{3\Delta_{\alpha} \Delta_{\beta}}{\ell^5} -
+            \frac{\delta_{\alpha\beta}}{\ell^3}
+        \right)
+        \kappa\
+        \ \ \alpha,\beta \in \{x,y,z\}
+
+where :math:`\rho` is density,
+:math:`\{x, y, z\}` correspond to the local coordinate system
+of the computation point P,
+:math:`\delta_{\alpha\beta}` is the `Kronecker delta`_, and
+
+.. math::
+   :nowrap:
+
+    \begin{eqnarray*}
+        \Delta_x &=& r' K_{\phi} \\
+        \Delta_y &=& r' \cos \phi' \sin(\lambda' - \lambda) \\
+        \Delta_z &=& r' \cos \psi - r\\
+        \ell &=& \sqrt{r'^2 + r^2 - 2 r' r \cos \psi} \\
+        \cos\psi &=& \sin\phi\sin\phi' + \cos\phi\cos\phi'
+                     \cos(\lambda' - \lambda) \\
+        K_{\phi} &=& \cos\phi\sin\phi' - \sin\phi\cos\phi'
+                     \cos(\lambda' - \lambda)\\
+        \kappa &=& {r'}^2 \cos \phi'
+    \end{eqnarray*}
+
+
+:math:`\phi` is latitude,
+:math:`\lambda` is longitude, and
+:math:`r` is radius.
+
+.. _Kronecker delta: http://en.wikipedia.org/wiki/Kronecker_delta
+
+**Numerical integration**
+
+The above integrals are solved using the Gauss-Legendre Quadrature rule
+(Asgharzadeh et al., 2007;
+Wild-Pfeiffer, 2008):
+
+.. math::
+    g_{\alpha\beta}(r,\phi,\lambda) \approx G \rho
+        \frac{(\lambda_2 - \lambda_1)(\phi_2 - \phi_1)(r_2 - r_1)}{8}
+        \displaystyle\sum_{k=1}^{N^{\lambda}}
+        \displaystyle\sum_{j=1}^{N^{\phi}}
+        \displaystyle\sum_{i=1}^{N^r}
+        W^r_i W^{\phi}_j W^{\lambda}_k
+        I_{\alpha\beta}({r'}_i, {\phi'}_j, {\lambda'}_k )
+        \ \alpha,\beta \in \{1,2,3\}
+
+where :math:`W_i^r`, :math:`W_j^{\phi}`, and :math:`W_k^{\lambda}`
+are weighting coefficients
+and :math:`N^r`, :math:`N^{\phi}`, and :math:`N^{\lambda}`
+are the number of quadrature nodes
+(i.e., the order of the quadrature),
+for the radius, latitude, and longitude, respectively.
+
+
+**References**
+
+Asgharzadeh, M. F., R. R. B. von Frese, H. R. Kim, T. E. Leftwich,
+and J. W. Kim (2007),
+Spherical prism gravity effects by Gauss-Legendre quadrature integration,
+Geophysical Journal International, 169(1), 1-11,
+doi:10.1111/j.1365-246X.2007.03214.x.
+
+Grombein, T.; Seitz, K.; Heck, B. (2013), Optimized formulas for the
+gravitational field of a tesseroid, Journal of Geodesy,
+doi: 10.1007/s00190-013-0636-1
+
+Wild-Pfeiffer, F. (2008),
+A comparison of different mass elements for use in gravity gradiometry,
+Journal of Geodesy, 82(10), 637-653, doi:10.1007/s00190-008-0219-8.
+
+
+----
+
 """
 import numpy
 
