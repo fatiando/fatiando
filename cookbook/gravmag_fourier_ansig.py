3c1f46908ae0163efd78a34b93a2db8f43a4055b
Leonardo Uieda <leouieda@gmail.com>
2014-12-16 17:16:24 -0200

    Better plots for ansig recipe

--- cookbook/gravmag_fourier_ansig.py
+++ cookbook/gravmag_fourier_ansig.py
@@ -9,25 +9,23 @@ model = [mesher.Prism(-100, 100, -100, 100, 0, 2000, {'magnetization': 10})]
 area = (-5000, 5000, -5000, 5000)
 shape = (100, 100)
 z0 = -500
-xp, yp, zp = gridder.regular(area, shape, z=z0)
+x, y, z = gridder.regular(area, shape, z=z0)
 inc, dec = -30, 0
-tf = utils.contaminate(prism.tf(xp, yp, zp, model, inc, dec), 0.001,
+tf = utils.contaminate(prism.tf(x, y, z, model, inc, dec), 0.001,
                        percent=True)
-
-# Need to convert gz to SI units so that the result is also in SI
-ansig = fourier.ansig(xp, yp, utils.nt2si(tf), shape)
+ansig = fourier.ansig(x, y, tf, shape)
 
 mpl.figure()
 mpl.subplot(1, 2, 1)
 mpl.title("Original total field anomaly")
 mpl.axis('scaled')
-mpl.contourf(yp, xp, tf, shape, 30)
-mpl.colorbar(orientation='horizontal')
+mpl.contourf(y, x, tf, shape, 30, cmap=mpl.cm.RdBu_r)
+mpl.colorbar(orientation='horizontal').set_label('nT')
 mpl.m2km()
 mpl.subplot(1, 2, 2)
 mpl.title("Analytic signal")
 mpl.axis('scaled')
-mpl.contourf(yp, xp, ansig, shape, 30)
-mpl.colorbar(orientation='horizontal')
+mpl.contourf(y, x, ansig, shape, 30, cmap=mpl.cm.RdBu_r)
+mpl.colorbar(orientation='horizontal').set_label('nT/m')
 mpl.m2km()
 mpl.show()
