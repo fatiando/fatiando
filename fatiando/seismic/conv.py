c7013ab4eb0b327a686aef8e9298996bd591da5c
victortxa <victortxa@gmail.com>
2015-04-01 16:50:38 -0300

    Optional density input model added to the program

--- fatiando/seismic/conv.py
+++ fatiando/seismic/conv.py
@@ -71,9 +71,33 @@ def seismic_convolutional_model(n_samples, n_traces, model, f, dz=1.,
             vel_l[jj, j] = vel[resmpl*jj, j]  # 10=dt/dt_new, dt_new=0.002=2ms
     for j in range(1, len(TWT_ts)):
         TWT_ts[j, :] = TWT_rs[resmpl*j]
+    # density calculations
+    if isinstance(rho, np.ndarray):
+        rho2 = np.ones((np.ceil(TMAX/dt_dwn), n_traces))
+        for j in range(0, n_traces):
+            kk = np.ceil(TWT[0, j]/dt_dwn)
+            lim = np.ceil(TWT[-1, j]/dt_dwn)-1
+    # linear interpolation
+            tck = interpolate.interp1d(TWT[:, j], rho[:, j])
+            rho2[kk:lim, j] = tck(TWT_rs[kk:lim])
+    # extension of the model repeats the last value of the true model
+            rho2[lim:, j] = rho2[lim-1, j]
+    # first values equal to the first value of the true model
+            rho2[0:kk, j] = rho[0, j]
+        # resampling from dt_dwn to dt
+        rho_l = np.zeros((np.ceil(TMAX/dt), n_traces))
+        resmpl = int(dt/dt_dwn)
+        rho_l[0, :] = rho2[0, :]
+        for j in range(0, n_traces):
+            for jj in range(1, len(TWT_ts)):
+                rho_l[jj, j] = rho[resmpl*jj, j]
     # calculate RC
-    rc = np.zeros(np.shape(vel_l))
-    rc[1:, :] = (vel_l[1:, :]-vel_l[:-1, :])/(vel_l[1:, :]+vel_l[:-1, :])
+    if ~isinstance(rho, np.ndarray):
+        rc = np.zeros(np.shape(vel_l))
+        rc[1:, :] = (vel_l[1:, :]-vel_l[:-1, :])/(vel_l[1:, :]+vel_l[:-1, :])
+    else:
+        rc[1:, :] = (vel_l[1:, :]*rho_l[1:, :]-vel_l[:-1, :]*rho_l[:-1, :])
+                    /(vel_l[1:, :]*rho_l[1:, :]+vel_l[:-1, :]*rho_l[:-1, :])
     #
     # wavelet
     w = rickerwave(f, dt)
