72a3de6d232084020a6ec332bc0a38443341ac1a
Leonardo Uieda <leouieda@gmail.com>
2015-04-24 18:47:37 -0300

    Rename the fourier tests to transform

--- test/test_gravmag_transform.py
+++ test/test_gravmag_transform.py
@@ -1,7 +1,7 @@
 from __future__ import division
 import numpy as np
 from numpy.testing import assert_allclose
-from fatiando.gravmag import fourier, prism
+from fatiando.gravmag import transform, prism
 from fatiando import gridder, utils
 from fatiando.mesher import Prism
 
@@ -12,7 +12,7 @@ def _trim(array, shape, d=20):
 
 
 def test_horizontal_derivatives_fd():
-    "gravmag.fourier 1st xy derivatives by finite diff against analytical"
+    "gravmag.transform 1st xy derivatives by finite diff against analytical"
     model = [Prism(-1000, 1000, -500, 500, 0, 2000, {'density': 100})]
     shape = (300, 300)
     x, y, z = gridder.regular([-10000, 10000, -10000, 10000], shape, z=-100)
@@ -23,8 +23,8 @@ def test_horizontal_derivatives_fd():
     grav = utils.mgal2si(prism.gz(x, y, z, model))
     for deriv in derivatives:
         analytical = getattr(prism, 'g{}z'.format(deriv))(x, y, z, model)
-        calculated = utils.si2eotvos(
-            getattr(fourier, 'deriv' + deriv)(x, y, grav, shape, method='fd'))
+        func = getattr(transform, 'deriv' + deriv)
+        calculated = utils.si2eotvos(func(x, y, grav, shape, method='fd'))
         diff = _trim(np.abs(analytical - calculated), shape)
         assert np.all(diff <= 0.005*np.abs(analytical).max()), \
             "Failed for g{}. Max: {} Mean: {} STD: {}".format(
@@ -32,20 +32,20 @@ def test_horizontal_derivatives_fd():
 
 
 def test_derivatives_uneven_shape():
-    "gravmag.fourier FFT derivatives work if grid spacing is uneven"
+    "gravmag.transform FFT derivatives work if grid spacing is uneven"
     model = [Prism(-1000, 1000, -500, 500, 0, 2000, {'density': 100})]
     shape = (150, 300)
     x, y, z = gridder.regular([-10000, 10000, -10000, 10000], shape, z=-100)
     grav = utils.mgal2si(prism.gz(x, y, z, model))
     analytical = prism.gzz(x, y, z, model)
-    calculated = utils.si2eotvos(fourier.derivz(x, y, grav, shape))
+    calculated = utils.si2eotvos(transform.derivz(x, y, grav, shape))
     diff = _trim(np.abs(analytical - calculated), shape)
     assert np.all(diff <= 0.005*np.abs(analytical).max()), \
         "Failed for gzz"
 
 
 def test_gz_derivatives():
-    "gravmag.fourier FFT first derivatives of gz against analytical solutions"
+    "gravmag.transform FFT 1st derivatives of gz against analytical solutions"
     model = [Prism(-1000, 1000, -500, 500, 0, 2000, {'density': 100})]
     shape = (300, 300)
     x, y, z = gridder.regular([-10000, 10000, -10000, 10000], shape, z=-100)
@@ -54,14 +54,14 @@ def test_gz_derivatives():
     for deriv in derivatives:
         analytical = getattr(prism, 'g{}z'.format(deriv))(x, y, z, model)
         calculated = utils.si2eotvos(
-            getattr(fourier, 'deriv' + deriv)(x, y, grav, shape))
+            getattr(transform, 'deriv' + deriv)(x, y, grav, shape))
         diff = _trim(np.abs(analytical - calculated), shape)
         assert np.all(diff <= 0.005*np.abs(analytical).max()), \
             "Failed for g{}z".format(deriv)
 
 
 def test_gx_derivatives():
-    "gravmag.fourier FFT first derivatives of gx against analytical solutions"
+    "gravmag.transform FFT 1st derivatives of gx against analytical solutions"
     model = [Prism(-1000, 1000, -500, 500, 0, 2000, {'density': 100})]
     shape = (300, 300)
     x, y, z = gridder.regular([-10000, 10000, -10000, 10000], shape, z=-100)
@@ -70,14 +70,14 @@ def test_gx_derivatives():
     for deriv in derivatives:
         analytical = getattr(prism, 'gx{}'.format(deriv))(x, y, z, model)
         calculated = utils.si2eotvos(
-            getattr(fourier, 'deriv' + deriv)(x, y, grav, shape))
+            getattr(transform, 'deriv' + deriv)(x, y, grav, shape))
         diff = _trim(np.abs(analytical - calculated), shape)
         assert np.all(diff <= 0.005*np.abs(analytical).max()), \
             "Failed for gx{}".format(deriv)
 
 
 def test_gy_derivatives():
-    "gravmag.fourier FFT first derivatives of gy against analytical solutions"
+    "gravmag.transform FFT 1st derivatives of gy against analytical solutions"
     model = [Prism(-1000, 1000, -500, 500, 0, 2000, {'density': 100})]
     shape = (300, 300)
     x, y, z = gridder.regular([-10000, 10000, -10000, 10000], shape, z=-100)
@@ -90,14 +90,14 @@ def test_gy_derivatives():
             func = getattr(prism, 'gy{}'.format(deriv))
         analytical = func(x, y, z, model)
         calculated = utils.si2eotvos(
-            getattr(fourier, 'deriv' + deriv)(x, y, grav, shape))
+            getattr(transform, 'deriv' + deriv)(x, y, grav, shape))
         diff = _trim(np.abs(analytical - calculated), shape)
         assert np.all(diff <= 0.005*np.abs(analytical).max()), \
             "Failed for gy{}".format(deriv)
 
 
 def test_second_derivatives():
-    "gravmag.fourier FFT second derivatives against analytical solutions"
+    "gravmag.transform FFT second derivatives against analytical solutions"
     model = [Prism(-1000, 1000, -500, 500, 0, 2000, {'density': -200})]
     shape = (300, 300)
     x, y, z = gridder.regular([-10000, 10000, -10000, 10000], shape, z=-100)
@@ -106,7 +106,7 @@ def test_second_derivatives():
     for deriv in derivatives:
         analytical = getattr(prism, 'g{}'.format(deriv))(x, y, z, model)
         calculated = utils.si2eotvos(
-            getattr(fourier, 'deriv' + deriv[0])(x, y, pot, shape, order=2))
+            getattr(transform, 'deriv' + deriv[0])(x, y, pot, shape, order=2))
         diff = _trim(np.abs(analytical - calculated), shape)
         assert np.all(diff <= 0.005*np.abs(analytical).max()), \
             "Failed for g{}. Max: {} Mean: {} STD: {}".format(
@@ -114,14 +114,14 @@ def test_second_derivatives():
 
 
 def test_laplace_from_potential():
-    "gravmag.fourier second derivatives of potential obey the Laplace equation"
+    "gravmag.transform 2nd derivatives of potential obey the Laplace equation"
     model = [Prism(-1000, 1000, -500, 500, 0, 2000, {'density': 200})]
     shape = (300, 300)
     x, y, z = gridder.regular([-10000, 10000, -10000, 10000], shape, z=-100)
     potential = prism.potential(x, y, z, model)
-    gxx = utils.si2eotvos(fourier.derivx(x, y, potential, shape, order=2))
-    gyy = utils.si2eotvos(fourier.derivy(x, y, potential, shape, order=2))
-    gzz = utils.si2eotvos(fourier.derivz(x, y, potential, shape, order=2))
+    gxx = utils.si2eotvos(transform.derivx(x, y, potential, shape, order=2))
+    gyy = utils.si2eotvos(transform.derivy(x, y, potential, shape, order=2))
+    gzz = utils.si2eotvos(transform.derivz(x, y, potential, shape, order=2))
     laplace = _trim(gxx + gyy + gzz, shape)
     assert np.all(np.abs(laplace) <= 1e-10), \
         "Max: {} Mean: {} STD: {}".format(
