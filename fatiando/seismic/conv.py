0731a5589fd6856040868dcc63164453e966f3be
victortxa <victortxa@gmail.com>
2015-05-15 15:22:21 -0300

    Function splitted in more function and option to choose the wavelet

--- fatiando/seismic/conv.py
+++ fatiando/seismic/conv.py
@@ -11,7 +11,7 @@ which we advise ricker wavelet (rickerwave function).
 
 **References**
 
-Yilmaz, Öz,
+Yilmaz, Oz,
 Ch.2 Deconvolution. In: YILMAZ, Öz. Seismic Data Analysis: Processing, 
 Inversion, and Interpretation of Seismic Data. Tulsa: Seg, 2001. Cap. 2. 
 p. 159-270. Available at: <http://dx.doi.org/10.1190/1.9781560801580.ch2>
@@ -25,14 +25,13 @@ import numpy as np
 from scipy import interpolate  # linear interpolation of velocity/density
 
 
-def seismic_convolutional_model(n_samples, n_traces, model, f, dz=1.,
-                                dt=2.e-3, rho=1.):
+def seismic_convolutional_model(n_traces, model, f, dt=2.e-3, rho=1.,wavelet):
     """
     Calculate convolutional seismogram for a geological model    
     
-    Calculate the synthetic concolutionla seismogram of a geological depth 
-    model, Vp is mandatory while density is optional. The given model in a
-    matrix form is considered a mesh of square cells.
+    Calculate the synthetic convolutional seismogram of a geological model, Vp
+    is mandatory while density is optional. The given model in a matrix form is
+    considered a mesh of square cells.
 
     .. warning::
 
@@ -42,23 +41,66 @@ def seismic_convolutional_model(n_samples, n_traces, model, f, dz=1.,
 
     Parameters:
 
-    * n_samples, n_traces: integers
+    * n_traces: integers
         The vertical and horizontal grid dimensions
     * model : 2D-array
         Vp values
     * f : float
         Dominant frequency of the ricker wavelet
-    * dz : float
-        Length of square grid cells
     * dt: float
         Sample time of the ricker wavelet and of the resulting seismogram
+    * wavelet : float
+        The function to consider as source in the seismic modelling.
 
     Returns:
 
     * synth_l : 2D-array
         Resulting seismogram
+
+    """ 
+    #dimension of rho must be the same of velocity grid, if both are matrix
+    if isinstance(rho, np.ndarray):
+        try:
+            np.shape(rho)==np.shape(vel_l)
+        except Exception as error:
+            print error
+    # calculate RC
+    if not isinstance(rho, np.ndarray):
+        rc = np.zeros(np.shape(vel_l))
+        rc[1:, :] = (vel_l[1:, :]-vel_l[:-1, :])/(vel_l[1:, :]+vel_l[:-1, :])
+    else:
+        rc[1:, :] = ((vel_l[1:, :]*rho[1:, :]-vel_l[:-1, :]*rho[:-1, :]) /
+                     (vel_l[1:, :]*rho[1:, :]+vel_l[:-1, :]*rho[:-1, :]))
+    w = wavelet(f, dt)
+    # convolution
+    synth_l = np.zeros(np.shape(rc))
+    for j in range(0, n_traces):
+        if np.shape(rc)[0] >= len(w):
+            synth_l[:, j] = np.convolve(rc[:, j], w, mode='same')
+        else:
+            aux = np.floor(len(w)/2.)
+            synth_l[:, j] = np.convolve(rc[:, j], w, mode='full')[aux:-aux]
+    return synth_l
+
+def depth_2_time(n_samples, n_traces, model, dt=2.e-3, dz=1., rho=1.):
+    """
+    Convert depth property model to time model.
+
+    Parameters:
+    * n_samples, n_traces: integers
+        The vertical and horizontal grid dimensions
+    * model : 2D-array
+        Vp values
+    * dt: float
+        Sample time of the ricker wavelet and of the resulting seismogram
+    * dz : float
+        Length of square grid cells
+
+    Returns:
+
     * TWT_ts : 1D-array
-        Time axis for the seismogram
+        Time axis for the property converted
+
     """
     dt_dwn = dt/10.
     TWT = np.zeros((n_samples, n_traces))
@@ -68,7 +110,6 @@ def seismic_convolutional_model(n_samples, n_traces, model, f, dz=1.,
     TMAX = max(TWT[-1, :])
     #
     TMIN = min(TWT[0, :])
-# if dt/TMIN
     TWT_rs = np.zeros(np.ceil(TMAX/dt_dwn))
     for j in range(1, len(TWT_rs)):
         TWT_rs[j] = TWT_rs[j-1]+dt_dwn
@@ -99,41 +140,20 @@ def seismic_convolutional_model(n_samples, n_traces, model, f, dz=1.,
         for j in range(0, n_traces):
             kk = np.ceil(TWT[0, j]/dt_dwn)
             lim = np.ceil(TWT[-1, j]/dt_dwn)-1
-    # linear interpolation
             tck = interpolate.interp1d(TWT[:, j], rho[:, j])
             rho2[kk:lim, j] = tck(TWT_rs[kk:lim])
-    # extension of the model repeats the last value of the true model
             rho2[lim:, j] = rho2[lim-1, j]
-    # first values equal to the first value of the true model
             rho2[0:kk, j] = rho[0, j]
-        # resampling from dt_dwn to dt
+     # resampling from dt_dwn to dt
         rho_l = np.zeros((np.ceil(TMAX/dt), n_traces))
         resmpl = int(dt/dt_dwn)
         rho_l[0, :] = rho2[0, :]
         for j in range(0, n_traces):
             for jj in range(1, len(TWT_ts)):
                 rho_l[jj, j] = rho[resmpl*jj, j]
-    # calculate RC
-    if not isinstance(rho, np.ndarray):
-        rc = np.zeros(np.shape(vel_l))
-        rc[1:, :] = (vel_l[1:, :]-vel_l[:-1, :])/(vel_l[1:, :]+vel_l[:-1, :])
-    else:
-        rc[1:, :] = ((vel_l[1:, :]*rho_l[1:, :]-vel_l[:-1, :]*rho_l[:-1, :]) /
-                     (vel_l[1:, :]*rho_l[1:, :]+vel_l[:-1, :]*rho_l[:-1, :]))
-    #
-    # wavelet
-    w = rickerwave(f, dt)
-    #
-    # convolution
-    synth_l = np.zeros(np.shape(rc))
-    for j in range(0, n_traces):
-        if np.shape(rc)[0] >= len(w):
-            synth_l[:, j] = np.convolve(rc[:, j], w, mode='same')
         else:
-            aux = np.floor(len(w)/2.)
-            synth_l[:, j] = np.convolve(rc[:, j], w, mode='full')[aux:-aux]
-    return synth_l, TWT_ts
-
+            rho_l=rho
+    return vel_l,TWT_ts,rho_l
 
 def rickerwave(f, dt):
     """
