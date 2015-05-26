43a92693e93d1fccf1aabf74c475a1b2e2ba2fe4
victortxa <victortxa@gmail.com>
2015-05-26 16:48:42 -0300

    Functions organized, module working with an example. Cookbook ans tests are the next steps.

--- fatiando/seismic/conv.py
+++ fatiando/seismic/conv.py
@@ -59,7 +59,7 @@ def seismic_convolutional_model(n_traces, vel_l, f, wavelet, dt=2.e-3, rho=1.):
     * synth_l : 2D-array
         Resulting seismogram
 
-    """ 
+    """
     # calculate RC
     rc = np.zeros(np.shape(vel_l))
     try:
@@ -100,17 +100,38 @@ def depth_2_time(n_samples, n_traces, model, dt=2.e-3, dz=1., rho=1.):
         Time axis for the property converted
 
     """
+    #downsampled time rate to make a better interpolation
     dt_dwn = dt/10.
     TWT = np.zeros((n_samples, n_traces))
     TWT[0, :] = 2*dz/model[0, :]
     for j in range(1, n_samples):
         TWT[j, :] = TWT[j-1]+2*dz/model[j, :]
     TMAX = max(TWT[-1, :])
-    #
     TMIN = min(TWT[0, :])
     TWT_rs = np.zeros(np.ceil(TMAX/dt_dwn))
     for j in range(1, len(TWT_rs)):
         TWT_rs[j] = TWT_rs[j-1]+dt_dwn
+    resmpl = int(dt/dt_dwn)
+    vel_l=_resampling(model,TMAX,TWT,TWT_rs,dt,dt_dwn,n_traces)
+    TWT_ts = np.zeros((np.ceil(TMAX/dt), n_traces))
+    for j in range(1, len(TWT_ts)):
+        TWT_ts[j, :] = TWT_rs[resmpl*j]
+    # density calculations
+    try:
+       rho_l=_resampling(rho,TMAX,TWT,TWT_rs,dt,dt_dwn,n_traces)
+    except TypeError:
+        rho_l = rho
+    return vel_l,TWT_ts,rho_l
+
+def _resampling(model,TMAX,TWT,TWT_rs,dt,dt_dwn,n_traces):
+    """
+    Resamples the input data to adjust it after time conversion with the chosen
+    time sample rate, dt. 
+    
+    Returns:
+
+    * vel_l : resampled input data
+    """ 
     vel = np.ones((np.ceil(TMAX/dt_dwn), n_traces))
     for j in range(0, n_traces):
         kk = np.ceil(TWT[0, j]/dt_dwn)
@@ -124,36 +145,12 @@ def depth_2_time(n_samples, n_traces, model, dt=2.e-3, dz=1., rho=1.):
         vel[0:kk, j] = model[0, j]
     # resampling from dt_dwn to dt
     vel_l = np.zeros((np.ceil(TMAX/dt), n_traces))
-    TWT_ts = np.zeros((np.ceil(TMAX/dt), n_traces))
     resmpl = int(dt/dt_dwn)
     vel_l[0, :] = vel[0, :]
     for j in range(0, n_traces):
-        for jj in range(1, len(TWT_ts)):
-            vel_l[jj, j] = vel[resmpl*jj, j]  # 10=dt/dt_new, dt_new=0.002=2ms
-    for j in range(1, len(TWT_ts)):
-        TWT_ts[j, :] = TWT_rs[resmpl*j]
-
-#block
-    # density calculations
-    if isinstance(rho, np.ndarray):
-        rho2 = np.ones((np.ceil(TMAX/dt_dwn), n_traces))
-        for j in range(0, n_traces):
-            kk = np.ceil(TWT[0, j]/dt_dwn)
-            lim = np.ceil(TWT[-1, j]/dt_dwn)-1
-            tck = interpolate.interp1d(TWT[:, j], rho[:, j])
-            rho2[kk:lim, j] = tck(TWT_rs[kk:lim])
-            rho2[lim:, j] = rho2[lim-1, j]
-            rho2[0:kk, j] = rho[0, j]
-     # resampling from dt_dwn to dt
-        rho_l = np.zeros((np.ceil(TMAX/dt), n_traces))
-        resmpl = int(dt/dt_dwn)
-        rho_l[0, :] = rho2[0, :]
-        for j in range(0, n_traces):
-            for jj in range(1, len(TWT_ts)):
-                rho_l[jj, j] = rho2[resmpl*jj, j]
-    else:
-        rho_l = rho
-    return vel_l,TWT_ts,rho_l
+        for jj in range(1, int(np.ceil(TMAX/dt))):
+            vel_l[jj, j] = vel[resmpl*jj, j]  # 10=dt/dt_new, dt_new=0.002=2m
+    return vel_l
 
 def rickerwave(f, dt):
     """
