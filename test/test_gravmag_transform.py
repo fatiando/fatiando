fd832f1a48b621032179793596e4e71735b731b1
Leonardo Uieda <leouieda@gmail.com>
2015-06-22 13:33:30 -0300

    Begin testing upcontinue. No luck.
    Can't figure out accuracy.

--- test/test_gravmag_transform.py
+++ test/test_gravmag_transform.py
@@ -148,3 +148,33 @@ def test_laplace_from_potential():
     assert np.all(np.abs(laplace) <= 1e-10), \
         "Max: {} Mean: {} STD: {}".format(
             laplace.max(), laplace.mean(), laplace.std())
+
+
+def test_upcontinue():
+    "gravmag.transform upward continuation matches analytical solution"
+    model = [Prism(-1000, 1000, -500, 500, 0, 1000,
+                   {'density': 500,
+                    'magnetization': utils.ang2vec(10, 20, -30)})]
+    shape = (100, 100)
+    inc, dec = -10, 15
+    x, y, z = gridder.regular([-5000, 5000, -5000, 5000], shape, z=-100)
+    dz = 20
+    fields = 'potential gx gy gz gxx gxy gxz gyy gyz gzz'.split()
+    accuracy = [(0.1, 0), (0.1, 0.1), (0.1, 0.1), (0.1, 0.1),
+                (0.1, 0.1), (0.1, 0.1), (0.1, 0.1),
+                (0.1, 0.1), (0.1, 0.1), (0.1, 0.1)]
+    for f, tmp in zip(fields, accuracy):
+        rtol, atol = tmp
+        func = getattr(prism, f)
+        data = func(x, y, z, model)
+        analytical = _trim(func(x, y, z + dz, model), shape, d=20)
+        for method in 'fft'.split():
+            up = transform.upcontinue(x, y, data, shape, dz, method=method)
+            up = _trim(up, shape, d=20)
+            diff = np.abs(up - analytical)
+            assert np.all(diff <= atol + rtol*np.abs(analytical)), \
+                'Failed for {} using the {} method'.format(f, method)
+            print(atol + rtol*np.abs(analytical))
+            #assert_allclose(up, analytical, rtol=rtol, atol=atol,
+                            #err_msg='Failed for {} using the {} method'.format(
+                                #f, method))
