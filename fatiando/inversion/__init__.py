f3bb588f8644b35796d49c9199f83c8944a678e4
Leonardo Uieda <leouieda@gmail.com>
2015-07-13 19:09:15 -0300

    Simplify the docstring for fatiando.inversion

--- fatiando/inversion/__init__.py
+++ fatiando/inversion/__init__.py
@@ -2,7 +2,7 @@
 Everything you need to solve inverse problems!
 
 This package provides the basic building blocks to implement inverse problem
-solvers. The main class for this is :class:`~fatiando.inversion.base.Misfit`.
+solvers. The main class for this is :class:`~fatiando.inversion.misfit.Misfit`.
 It represents a data-misfit function and contains various tools to fit a
 model to some data. All you have to do is implement methods to calculate the
 predicted (or modeled) data and (optionally) the Jacobian (or sensitivity)
@@ -12,20 +12,32 @@ regularization, joint inversion, etc.
 Modules
 -------
 
-* :mod:`~fatiando.inversion.base`: Base classes for building inverse problem
-  solvers
+* :mod:`~fatiando.inversion.misfit`: Defines the data-misfit classes. Used to
+  implement new inverse problems.
 * :mod:`~fatiando.inversion.regularization`: Classes for common regularizing
   functions and base classes for building new ones
-* :mod:`~fatiando.inversion.solvers`: Functions for several optimization
-  methods (used by :class:`~fatiando.inversion.base.Misfit`)
+* :mod:`~fatiando.inversion.hyper_param`: Classes hyper parameter optimization
+  (e.g., estimating the regularization parameter through an L-curve).
+* :mod:`~fatiando.inversion.optimization`: Functions for several optimization
+  methods (used by :class:`~fatiando.inversion.misfit.Misfit`)
+* :mod:`~fatiando.inversion.base`: Base classes used internally to define
+  common operations and method.
 
-Have a look at the examples bellow on how to use the package. More geophysical
-examples include :mod:`fatiando.seismic.srtomo`,
-:mod:`fatiando.seismic.profile`,
-:mod:`fatiando.gravmag.basin2d`,
-:mod:`fatiando.gravmag.eqlayer`,
-and
-:mod:`fatiando.gravmag.euler`.
+You can import the ``Misfit``, regularization, and hyper parameter optimization
+classes directly from the ``fatiando.inversion`` namespace:
+
+>>> from fatiando.inversion import Misfit, LCurve, Damping, Smoothness
+
+The :class:`~fatiando.inversion.misfit.Misfit` class is used internally in
+Fatiando to implement all of our inversion algorithms. Take a look at the
+modules below for some examples:
+
+* :mod:`fatiando.seismic.srtomo`
+* :mod:`fatiando.gravmag.basin2d`
+* :mod:`fatiando.gravmag.eqlayer`
+* :mod:`fatiando.gravmag.euler`
+* :mod:`fatiando.gravmag.magdir`
+* :mod:`fatiando.seismic.profile`
 
 ----
 
