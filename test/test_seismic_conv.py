ab8aa763da1938be738a5f71dd545eb651862404
victortxa <victortxa@gmail.com>
2015-08-11 18:53:20 -0300

    fixing input functions parameters for the tests from the last commit

--- test/test_seismic_conv.py
+++ test/test_seismic_conv.py
@@ -12,15 +12,15 @@ def test_density_matrix_input():
     rock_grid = 1500.*np.ones((n_samples, n_traces))
     rock_grid[300:,:] = 2500.
     #synthetic calculation for rho as int
-    [vel_l_int, rho_l_int] = conv.depth_2_time(n_samples, n_traces,
-                                                rock_grid, dt=2.e-3)
-    synt_int = conv.seismic_convolutional_model(n_traces, vel_l_int,
-                                                30., conv.rickerwave)
+    [vel_l_int, rho_l_int] = conv.depth_2_time(rock_grid, dt=2.e-3)
+    rc_int=conv.calc_RC(vel_l_int,rho_l_int)
+    synt_int = conv.seismic_convolutional_model(rc_int, 30., conv.rickerwave)
     #synthetic calculation for rho as matrix
-    [vel_l_mat,rho_l_mat] = conv.depth_2_time(n_samples, 
-           n_traces, rock_grid, dt=2.e-3,rho=1.*np.ones((n_samples, n_traces)))
-    synt_mat = conv.seismic_convolutional_model(n_traces, vel_l_mat,
-                                           30., conv.rickerwave, rho=rho_l_mat)
+    [vel_l_mat,rho_l_mat] = conv.depth_2_time(rock_grid, dt=2.e-3,
+                                        rho=1.*np.ones((n_samples, n_traces)))
+    rc_mat=conv.calc_RC(vel_l_mat,rho_l_mat)
+    synt_mat = conv.seismic_convolutional_model(rc_mat, 30., conv.rickerwave) 
+                                                
     assert_array_almost_equal(synt_int, synt_mat, 9)
 
 def test_impulse_response():
@@ -30,8 +30,8 @@ def test_impulse_response():
     equal to the source wavelet
     """
     w=conv.rickerwave(30., 2.e-3)
-    RC_test=np.zeros((w.shape, 20))
+    RC_test=np.zeros((w.shape[0], 20))
     RC_test[w.shape[0]/2,:]=1
-    spike=conv.seismic_convolutional_model(RC_test, 30., w)
+    spike=conv.seismic_convolutional_model(RC_test, 30., conv.rickerwave)
     for j in range(0,RC_test.shape[1]):
         assert_array_almost_equal(spike[:,j], w, 9)
