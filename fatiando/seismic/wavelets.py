2a66b2db6776170d02b9c02260e4f47ea75a418e
Leonardo Uieda <leouieda@gmail.com>
2016-09-02 10:39:51 -0300

    Implement Ricker and Gaussian wavelets
    As given in the paper by Di Bartolo et al (2012).

--- fatiando/seismic/wavelets.py
+++ fatiando/seismic/wavelets.py
@@ -1,14 +1,54 @@
 from __future__ import division, print_function
+from future.utils import with_metaclass
 from future.builtins import super, object, range
+from abc import ABCMeta, abstractmethod
 
 import numpy as np
 
 
-class RickerWavelet(object):
-    def __init__(self, amp, f_cut, delay=0):
+class BaseWavelet(with_metaclass(ABCMeta)):
+
+    def __init__(self, amp):
+        self.amp = amp
+
+    @abstractmethod
+    def __call__(self, time):
         pass
 
 
-class GaussianWavelet(object):
+class GaussianWavelet(BaseWavelet):
+
     def __init__(self, amp, f_cut, delay=0):
-        pass
+        super().__init__(amp)
+        self.f_cut = f_cut
+        self.delay = delay
+
+    def __call__(self, time):
+        sqrt_pi = np.sqrt(np.pi)
+        fc = self.f_cut/(3*sqrt_pi)
+        # Standard delay to make the wavelet start at time zero and be causal
+        td = time - 2*sqrt_pi/self.f_cut
+        # Apply the user defined delay on top
+        t = td - self.delay
+        scale = self.amp/(2*np.pi*(np.pi*fc)**2)
+        res = scale*np.exp(-np.pi*(np.pi*fc*t)**2)
+        return res
+
+
+class RickerWavelet(BaseWavelet):
+
+    def __init__(self, amp, f_cut, delay=0):
+        super().__init__(amp)
+        self.f_cut = f_cut
+        self.delay = delay
+
+    def __call__(self, time):
+        sqrt_pi = np.sqrt(np.pi)
+        fc = self.f_cut/(3*sqrt_pi)
+        # Standard delay to make the wavelet start at time zero and be causal
+        td = time - 2*sqrt_pi/self.f_cut
+        # Apply the user defined delay on top
+        t = td - self.delay
+        scale = self.amp*(2*np.pi*(np.pi*fc*t)**2 - 1)
+        res = scale*np.exp(-np.pi*(np.pi*fc*t)**2)
+        return res
