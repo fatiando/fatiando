b87c16f2df383bd2b1251c98ab908cedccff3831
Vanderlei C Oliveira Jr <vandscoelho@gmail.com>
2017-01-31 14:24:19 -0200

    correct pep8 problems

--- fatiando/tests/mesher/test_triaxial_ellipsoid.py
+++ fatiando/tests/mesher/test_triaxial_ellipsoid.py
@@ -4,8 +4,8 @@ from numpy.random import rand
 
 
 def test_triaxial_ellipsoid_copy():
-    orig = TriaxialEllipsoid(1, 2, 3, 6, 5, 4, 10, 20, 30, 
-                            {'remanence': [10000., 25., 40.], 
+    orig = TriaxialEllipsoid(1, 2, 3, 6, 5, 4, 10, 20, 30, {
+                             'remanence': [10000., 25., 40.],
                              'k': [0.562, 0.485, 0.25, 90., 0., 0.]})
     cp = orig.copy()
     assert orig is not cp
@@ -26,24 +26,26 @@ def test_triaxial_ellipsoid_copy():
     assert orig.y != cp.y
     assert orig.z != cp.z
     assert orig.props['k'] != cp.props['k']
-    
+
+
 def test_triaxial_ellipsoid_V_orthogonal():
     a = 1000.*rand()
     b = 0.8*a
     c = 0.6*a
     pi = np.pi
-    e = TriaxialEllipsoid(1000.*rand(), 1000.*rand(), 1000.*rand(), 
-                          a, b, c, 
+    e = TriaxialEllipsoid(1000.*rand(), 1000.*rand(), 1000.*rand(),
+                          a, b, c,
                           pi*rand(), 0.5*pi*rand(), 0.5*pi*rand())
-    assert np.allclose(np.dot(e.V().T,e.V()), np.identity(3))
-    assert np.allclose(np.dot(e.V(),e.V().T), np.identity(3))
+    assert np.allclose(np.dot(e.V().T, e.V()), np.identity(3))
+    assert np.allclose(np.dot(e.V(), e.V().T), np.identity(3))
+
 
 def test_triaxial_ellipsoid_V_identity():
     a = 1000.*rand()
     b = 0.8*a
     c = 0.6*a
     pi = np.pi
-    e = TriaxialEllipsoid(1000.*rand(), 1000.*rand(), 1000.*rand(), 
-                          a, b, c, 
+    e = TriaxialEllipsoid(1000.*rand(), 1000.*rand(), 1000.*rand(),
+                          a, b, c,
                           180., 90., 0.)
     assert np.allclose(e.V(), np.identity(3))
