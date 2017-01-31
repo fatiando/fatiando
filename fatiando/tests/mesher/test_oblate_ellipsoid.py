c5b294c794205bbddef377c0bd347a8ffd4228fb
Vanderlei C Oliveira Jr <vandscoelho@gmail.com>
2017-01-31 14:16:42 -0200

    correct pep8 problems

--- fatiando/tests/mesher/test_oblate_ellipsoid.py
+++ fatiando/tests/mesher/test_oblate_ellipsoid.py
@@ -4,9 +4,9 @@ from numpy.random import rand
 
 
 def test_oblate_ellipsoid_copy():
-    orig = OblateEllipsoid(1, 2, 3, 4, 6, 10, 30,
-                            {'remanence': [10000., 25., 40.], 
-                             'k': [0.562, 0.485, 0.25, 90., 0., 0.]})
+    orig = OblateEllipsoid(1, 2, 3, 4, 6, 10, 30, {
+        'remanence': [10000., 25., 40.],
+        'k': [0.562, 0.485, 0.25, 90., 0., 0.]})
     cp = orig.copy()
     assert orig is not cp
     assert orig.x == cp.x
@@ -25,23 +25,25 @@ def test_oblate_ellipsoid_copy():
     assert orig.y != cp.y
     assert orig.z != cp.z
     assert orig.props['remanence'] != cp.props['remanence']
-    
+
+
 def test_oblate_ellipsoid_V_orthogonal():
     a = 1000.*rand()
     b = 1.5*a
     pi = np.pi
-    e = OblateEllipsoid(1000.*rand(), 1000.*rand(), 1000.*rand(), 
-                          a, b, pi*rand(), 0.5*pi*rand())
-    assert np.allclose(np.dot(e.V().T,e.V()), np.identity(3))
-    assert np.allclose(np.dot(e.V(),e.V().T), np.identity(3))
+    e = OblateEllipsoid(1000.*rand(), 1000.*rand(), 1000.*rand(),
+                        a, b, pi*rand(), 0.5*pi*rand())
+    assert np.allclose(np.dot(e.V().T, e.V()), np.identity(3))
+    assert np.allclose(np.dot(e.V(), e.V().T), np.identity(3))
+
 
 def test_oblate_ellipsoid_V_specific():
     a = 1000.*rand()
     b = 2.4*a
     pi = np.pi
-    e = OblateEllipsoid(1000.*rand(), 1000.*rand(), 1000.*rand(), 
-                          a, b, 180., 90.)
+    e = OblateEllipsoid(1000.*rand(), 1000.*rand(), 1000.*rand(),
+                        a, b, 180., 90.)
     true = np.array([[-1, 0, 0],
-                     [ 0, 0,-1],
-                     [ 0,-1, 0]])
-    assert np.allclose(e.V(),true)
+                     [0, 0, -1],
+                     [0, -1, 0]])
+    assert np.allclose(e.V(), true)
